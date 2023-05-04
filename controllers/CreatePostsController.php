3aa66b26f266e16be46ae6b7af0b56d963b7418a
Axel92290 <81574847+Axel92290@users.noreply.github.com>
2023-05-04 14:44:40 +0200

    Posts
    - Update Controllers
    - Update Models
    - Create Branch
    - Create posts
    - listing posts

--- controllers/CreatePostsController.php
+++ controllers/CreatePostsController.php
@@ -8,11 +8,24 @@ class CreatePostsController extends BaseController
         // on choisi la template à appeler
         $template = $this->twig->load('create-posts/create.html');
 
-        // $post = new Post();
-        // $listPost = $post->getPosts();
-
 
         // Puis on affiche la page avec la méthode render
         echo $template->render(['title' => 'Création d\'un post']);
+
+        if (!empty($_POST['titre']) && !empty($_POST['contenu']) && !empty($_POST['submit'])) {
+
+
+
+            $title = $_POST['titre'];
+            $title = ucfirst(trim($title));
+            $content = $_POST['contenu'];
+            $content = ucfirst(trim($content));
+            $dateCreation = date('Y-m-d H:i:s');
+            $dateModification = date('Y-m-d H:i:s');
+            $idUser = $_SESSION['id'];
+
+            $post = new Post();
+            $post->createPost($title, $content, $dateCreation, $dateModification, $idUser);
+        }
     }
 }
--- models/Post.php
+++ models/Post.php
@@ -2,11 +2,16 @@
 
 class Post extends Database
 {
-    // public function getPosts()
-    // {
-    //     $posts = $this->connexion->query('SELECT * FROM posts');
-    //     return $posts;
-    // }
 
+    public function createPost($title, $content, $dateCreation, $dateModification, $idUser)
+    {
+        $req = $this->connexion->prepare("INSERT INTO posts(titre, contenu, dateCreation, dateModification, idUser ) VALUES (?, ?, ?, ?, ?)");
+        $req->execute(array($title, $content, $dateCreation, $dateModification, $idUser));
+    }
 
+    public function getPosts()
+    {
+        $posts = $this->connexion->query('SELECT * FROM posts ORDER BY ordre ASC');
+        return $posts;
+    }
 }
