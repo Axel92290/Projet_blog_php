d3ce08e50336db18465ffc63b2a5abb0e6f0385e
Axel92290 <axel.chasseloup@gmail.com>
2023-05-12 01:48:12 +0200

    Merge Post's Datas

--- controllers/CreatePostsController.php
+++ controllers/CreatePostsController.php
@@ -1,28 +1,55 @@
 <?php
 
+namespace Controllers;
+
+use Models\Post;
+
 class CreatePostsController extends BaseController
 {
+    /**
+     * @var string
+     */
+    private string $errors = '';
+    
     public function createPost()
     {
+
 
         // on choisi la template à appeler
         $template = $this->twig->load('create-posts/create.html');
 
 
         // Puis on affiche la page avec la méthode render
+
         echo $template->render(['title' => 'Création d\'un post']);
 
 
-        $post = new Post();
+        echo $template->render([
+            'title' => 'Création d\'un post',
+            'errors' => $this->errors
 
-        if (isset($_REQUEST['creer'])) {
+        ]);
 
-            $title = $_REQUEST['title'];
-            $content = $_REQUEST['content'];
+        if (!empty($_POST['titre']) && !empty($_POST['contenu']) && !empty($_POST['creer'])) {
+
+
+
+            $title = $_POST['titre'];
+            $title = ucfirst(trim($title));
+            $content = $_POST['contenu'];
+            $content = ucfirst(trim($content));
             $dateCreation = date('Y-m-d H:i:s');
             $dateModification = date('Y-m-d H:i:s');
-            $id_user = 1;
-            $post->insertPost($title, $content, $dateCreation, $dateModification, $id_user);
+            $idUser = $_SESSION['id'];
+
+            $post = new Post();
+            $post->createPost($title, $content, $dateCreation, $dateModification, $idUser);
+        } else {
+            $this->errors = 'Veuillez remplir tous les champs';
+
+            echo $template->render(['title' => 'Création d\'un post']);
         }
+
+
     }
 }
--- controllers/ListingController.php
+++ controllers/ListingController.php
@@ -1,15 +1,37 @@
 <?php
+namespace Controllers;
+
+use Models\Post;
 
 class ListingController extends BaseController
 {
+    /**
+     * @var string
+     */
+    private string $errors = '';
+    
     public function listing()
     {
 
         // on choisi la template à appeler
         $template = $this->twig->load('listing-posts/listing.html');
 
+        
+        $post = new Post();
+        $listPost = $post->getPosts();
+
 
         // Puis on affiche la page avec la méthode render
+
         echo $template->render(['title' => 'Liste des posts']);
+
+        echo $template->render([
+            'title' => 'Liste des posts',
+            'listPost' => $listPost,
+            'errors' => $this->errors
+        ]);
+
+
+
     }
 }
