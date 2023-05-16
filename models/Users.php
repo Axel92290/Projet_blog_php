297890727f183e27ffba163ab56621e8c5663311
Axel92290 <axel.chasseloup@gmail.com>
2023-05-16 21:25:07 +0200

    Update 16/05
    - Add exceptions (try/catch) for Model Database.php & Users.php

--- models/Users.php
+++ models/Users.php
@@ -11,10 +11,17 @@ class Users extends Database
      */
     public function loadUserByEmail($email): mixed
     {
+        try{
+            $stmt = $this->connexion->prepare('SELECT * FROM users WHERE email = :email');
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
 
-        $stmt = $this->connexion->prepare('SELECT * FROM users WHERE email = :email');
-        $stmt->execute(array('email' => $email));
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        }catch (\PDOException $e){
+            echo $e->getMessage();
+        }
+
+
     }
 
     /**
@@ -24,44 +31,64 @@ class Users extends Database
      */
     public function insertData($nom, $prenom, $mail, $pwd, $createdAt, $updatedAt): bool
     {
-        $stmt = $this->connexion->prepare('INSERT INTO users (firstname, lastname, email, pwd, createdAt, updatedAt) VALUES (:firstname, :lastname, :email, :pwd , :createdAt, :updatedAt)');
-        $stmt->bindValue('firstname', $prenom);
-        $stmt->bindValue('lastname', $nom);
-        $stmt->bindValue('email', $mail);
-        $stmt->bindValue('pwd', $pwd);
-        $stmt->bindValue('createdAt', $createdAt);
-        $stmt->bindValue('updatedAt', $updatedAt);
-        return $stmt->execute();
+        try{
+            $stmt = $this->connexion->prepare('INSERT INTO users (firstname, lastname, email, pwd, createdAt, updatedAt) VALUES (:firstname, :lastname, :email, :pwd , :createdAt, :updatedAt)');
+            $stmt->bindValue('firstname', $prenom);
+            $stmt->bindValue('lastname', $nom);
+            $stmt->bindValue('email', $mail);
+            $stmt->bindValue('pwd', $pwd);
+            $stmt->bindValue('createdAt', $createdAt);
+            $stmt->bindValue('updatedAt', $updatedAt);
+            return $stmt->execute();
+
+        }catch (\PDOException $e){
+            echo $e->getMessage();
+        }
+
     }
 
     public function updateDateConnexion($mail, $updatedAt) : mixed
     {
+        try{
+            $req = $this->connexion->prepare("UPDATE utilisateur SET updatedAt = ? WHERE mail = ?");
+            $req->bindValue('updatedAt', $updatedAt);
+            $req->bindValue('mail', $mail);
+            return $req->execute();
+        }catch (\PDOException $e){
+            echo $e->getMessage();
+        }
 
-        $req = $this->connexion->prepare("UPDATE utilisateur SET updatedAt = ? WHERE mail = ?");
-        $req->bindValue('updatedAt', $updatedAt);
-        $req->bindValue('mail', $mail);
-        return $req->execute();
+
     }
 
     public function checkUserByEmail($mail) : mixed
     {
+        try{
+            $req = $this->connexion->prepare("SELECT id FROM utilisateur WHERE mail = ?");
+            $req->bindValue('mail', $mail);
+            $req->execute();
+            $result = $req->fetch(PDO::FETCH_ASSOC);
+            return $result;
+        }catch(\PDOException $e){
+            echo $e->getMessage();
+        }
 
-        $req = $this->connexion->prepare("SELECT id FROM utilisateur WHERE mail = ?");
-        $req->bindValue('mail', $mail);
-        $req->execute();
-        $result = $req->fetch(PDO::FETCH_ASSOC);
-        return $result;
+
     }
 
     public function checkConnexion($mail, $pwd) : mixed
     {
+        try{
+            $req = $this->connexion->prepare("SELECT * FROM utilisateur WHERE mail = ? AND pword = ?");
+            $req->bindValue('mail', $mail);
+            $req->bindValue('pwd', $pwd);
+            $req->execute();
+            $result = $req->fetch(PDO::FETCH_CLASS);
+            return $result;
+        }catch(\PDOException $e){
+            echo $e->getMessage();
+        }
 
-        $req = $this->connexion->prepare("SELECT * FROM utilisateur WHERE mail = ? AND pword = ?");
 
-        $req->bindValue('mail', $mail);
-        $req->bindValue('pwd', $pwd);
-        $req->execute();
-        $result = $req->fetch(PDO::FETCH_CLASS);
-        return $result;
     }
 }
