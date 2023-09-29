ed270f7b6b9afb1d956d0b4d26a7b7428a36725b
Axel92290 <axel.chasseloup@gmail.com>
2023-09-29 19:32:26 +0200

    Bug

--- controllers/EditPostController.php
+++ controllers/EditPostController.php
@@ -22,6 +22,7 @@ class EditPostController extends BaseController
         // Vérifie la session de l'utilisateur.
         $this->checkSession();
 
+
         // Vérifie le jeton CSRF.
         $csrf = new \ParagonIE\AntiCSRF\AntiCSRF;
 
@@ -34,45 +35,47 @@ class EditPostController extends BaseController
         // Vérifie le rôle de l'utilisateur.
         $this->checkRole($userRole, $userId, $postUserId);
 
+
         if (!$this->httpRequest->isMethod('POST') || !$csrf->validateRequest()) {
             // Gère la méthode HTTP invalide ou le jeton CSRF incorrect ici.
-            return;
+
+
+            // Choisi la template à appeler.
+            $template = $this->twig->load('admin/edit.html');
+
+            // Affiche la page avec la méthode render.
+            $render = $template->render([
+                'title' => 'Edition du post',
+                'detailPost' => $detailPost[0],
+                'errors' => $this->errors,
+            ]);
+
+
+            print_r($render);
+
+            if (!$this->httpRequest->request->get('title')) {
+                $this->errors[] = 'Veuillez remplir le champ titre';
+            } elseif (!$this->httpRequest->request->get('content')) {
+                $this->errors[] = 'Veuillez remplir le champ contenu';
+            } elseif (!$this->httpRequest->request->get('chapo')) {
+                $this->errors[] = 'Veuillez remplir le champ chapo';
+            } else {
+                // Nettoie et récupère les données du formulaire.
+                $titre = ucfirst($this->cleanXSS($this->httpRequest->request->get('title')));
+                $chapo = ucfirst($this->cleanXSS($this->httpRequest->request->get('chapo')));
+                $contenu = ucfirst($this->cleanXSS($this->httpRequest->request->get('content')));
+
+                // Met à jour les données du post.
+                $this->updatePostData($titre, $chapo, $contenu, $id);
+
+                // Redirige vers la page des détails du post.
+                $this->redirect("/details-posts/$id");
+                return;
+            }
         }
-
-        if (!$this->httpRequest->request->get('title')) {
-            $this->errors[] = 'Veuillez remplir le champ titre';
-        } elseif (!$this->httpRequest->request->get('content')) {
-            $this->errors[] = 'Veuillez remplir le champ contenu';
-        } elseif (!$this->httpRequest->request->get('chapo')) {
-            $this->errors[] = 'Veuillez remplir le champ chapo';
-        } else {
-            // Nettoie et récupère les données du formulaire.
-            $titre = ucfirst($this->cleanXSS($this->httpRequest->request->get('title')));
-            $chapo = ucfirst($this->cleanXSS($this->httpRequest->request->get('chapo')));
-            $contenu = ucfirst($this->cleanXSS($this->httpRequest->request->get('content')));
-
-            // Met à jour les données du post.
-            $this->updatePostData($titre, $chapo, $contenu, $id);
-
-            // Redirige vers la page des détails du post.
-            $this->redirect("/details-posts/$id");
-            return;
-        }
-
-        // Choisi la template à appeler.
-        $template = $this->twig->load('admin/edit.html');
-
-        // Affiche la page avec la méthode render.
-        $render = $template->render([
-            'title' => 'Edition du post',
-            'detailPost' => $detailPost[0],
-            'errors' => $this->errors,
-        ]);
-
-        print_r($render);
     } // End editPost().
 
-    
+
     /**
      * Vérifie le rôle de l'utilisateur pour l'édition et la suppression d'un post.
      *
@@ -132,7 +135,6 @@ class EditPostController extends BaseController
         $post = new Post();
         $detailPost = $post->getPosts($id);
         return $detailPost;
-
     } // End getPost().
 
 
@@ -153,6 +155,5 @@ class EditPostController extends BaseController
         // Met à jour les données du post.
         $post = new Post();
         $post->updatePost($titre, $chapo, $contenu, $id);
-        
     } // End updatePostData().
 } // End EditPostController().
